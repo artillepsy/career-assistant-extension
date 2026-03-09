619ae7a6dcf13daa863ac981c7e6220913f828a7
Artillepsy <artillepsy@gmail.com>
2026-03-10 00:14:44 +0100

    refactor: replace mozilla API with chrome API; add sidePanel open upon clicking extension icon.

--- web-extension/entrypoints/background.ts
+++ web-extension/entrypoints/background.ts
@@ -1,18 +1,38 @@
 export default defineBackground(() => {
   console.log('Hello background!', { id: browser.runtime.id });
-  browser.browserAction.onClicked.addListener(() => {
-    browser.sidebarAction.open();
+  browser.action.onClicked.addListener(async (tab) => {
+    browser.sidePanel.open({ windowId: tab?.windowId });
   });
 
-  browser.menus.create({
+  browser.contextMenus.create({
     id: 'analyze-job',
     title: 'Analyze Job',
     contexts: ['page'],
   });
 
-  browser.menus.onClicked.addListener((info, tab) => {
+  /*browser.contextMenus.create({
+    id: 'open-panel',
+    title: 'Open side panel',
+    contexts: ['all'],
+  });*/
+
+  browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === 'analyze-job') {
-      console.log(`page is selected, url: ${info.pageUrl}`);
+      /*console.log(`page is selected, url: ${info.pageUrl}`);
+
+      const results = browser.scripting.executeScript({
+        target: { tabId: tab.tabId },
+        func: () => {
+          const bodyText = document.body.innerText;
+          const html = document.documentElement.innerHTML;
+          return { bodyText, html };
+        },
+      });
+
+      const pageData = results[0].result;
+      console.log(`page content: ${pageData.bodyText.substring(0, 100)}`);*/
+    } else if (info.menuItemId === 'open-panel') {
+      browser.sidePanel.open({ windowId: tab.windowId });
     }
   });
 });
--- web-extension/wxt.config.ts
+++ web-extension/wxt.config.ts
@@ -4,13 +4,14 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    permissions: ['sidePanel', 'sidebarAction', 'menus', 'activeTab'],
+    permissions: ['sidePanel', 'contextMenus', 'activeTab', 'scripting', 'storage'],
+    host_permissions: ['<all_urls>'],
     action: {
       /*default_popup: 'entrypoints/popup/index.html',*/
       default_title: 'Click to open sidebar',
     },
-    sidebar_action: {
-      default_panel: 'entrypoints/sidepanel/index.html',
+    side_panel: {
+      default_path: 'entrypoints/sidepanel/index.html',
       default_title: 'My Extension Sidebar',
       default_icon: 'assets/icon-32.png',
     },
