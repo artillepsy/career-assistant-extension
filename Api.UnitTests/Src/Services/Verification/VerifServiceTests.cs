3dd070d677f330b74fb8692ddcb7b9857c7edccb
Artillepsy <artillepsy@gmail.com>
2026-04-11 18:02:58 +0200

    refactor: improve unit test for verification code generation

--- Api.UnitTests/Src/Services/Verification/VerifServiceTests.cs
+++ Api.UnitTests/Src/Services/Verification/VerifServiceTests.cs
@@ -8,10 +8,11 @@ public class VerifServiceTests(ITestOutputHelper testOutputHelper)
 	private readonly VerifService _verifService = new();
 
 	[Fact]
-	public void GenerateCode_GeneratesRandomCode_ShouldGenerateDigitsOnly()
+	public void GenerateCode_GeneratesRandomCode_ShouldGenerate6DigitsCodeOnly()
 	{
 		var code = _verifService.GenerateCode();
 		testOutputHelper.WriteLine($"Generated code: {code}");
 		Assert.Matches("^[0-9]+$", code);
+		Assert.Equal(6, code.Length);
 	}
 }
